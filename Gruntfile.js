5a86ffbdabfb256df28eb6dd3ac137ba30c62037
Tom Riley <tdriley@Toms-MacBook-Pro-2.local>
2016-01-28 03:08:47 +0000

    tweaks, line spaces etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= pkg.author %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+				build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
 
 };
